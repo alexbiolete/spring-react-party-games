7188ff26e6de523ea7f651d8e50c42e945095e9a
Alex <alexbiolete@pm.me>
2022-01-27 12:26:08 +0200

    Minor improvements for smaller screens

--- client/src/views/Room.js
+++ client/src/views/Room.js
@@ -12,7 +12,7 @@ const Room = () => {
 
   return (
     <div className="space-y-4">
-      <div className="w-full p-4 flex items-center justify-between">
+      <div className="w-full p-4 hidden md:flex items-center justify-between">
         <div className="px-2 flex-1 flex items-center justify-start space-x-3 text-gray-400">
           <div className="flex items-center space-x-0.5">
             {room.type === 'private' && (
@@ -84,6 +84,80 @@ const Room = () => {
               </span>
             </>
           )}
+        </div>
+      </div>
+      <div className="w-full p-4 flex md:hidden flex-col">
+        <div className="px-2">
+          <h1 className="font-light tracking-wider text-center text-xl uppercase">
+            {room.name}
+          </h1>
+        </div>
+        <div className="px-2 flex items-center justify-between space-x-3 text-gray-400">
+          <div className="flex-1 flex items-center justify-start space-x-0.5">
+            {room.type === 'private' && (
+              <>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                </svg>
+                <span className="mt-0.5 capitalize">
+                  {room.type}
+                </span>
+              </>
+            )}
+            {room.type === 'public' && (
+              <>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd" />
+                </svg>
+                <span className="mt-0.5 capitalize">
+                  {room.type}
+                </span>
+              </>
+            )}
+          </div>
+          <div className="flex-1 flex items-center justify-center space-x-1.5 mt-0.5 text-gray-400">
+            {room.state === 'playing' && (
+              <>
+                <div>
+                  <div className="w-2 h-2 animate-ping absolute bg-green-600 rounded-full"></div>
+                  <div className="w-2 h-2 relative bg-green-600 rounded-full"></div>
+                </div>
+                <span className="font-semibold tracking-wider uppercase text-green-600">
+                  {room.state}
+                </span>
+              </>
+            )}
+            {room.state === 'waiting' && (
+              <>
+                <div className="w-2 h-2 animate-pulse bg-yellow-600 rounded-full"></div>
+                <span className="font-semibold tracking-wider uppercase text-yellow-600">
+                  {room.state}
+                </span>
+              </>
+            )}
+            {room.state === 'offline' && (
+              <>
+                <div className="w-2 h-2 bg-transparent border border-red-700 rounded-full"></div>
+                <span className="font-semibold tracking-wider uppercase text-red-700">
+                  {room.state}
+                </span>
+              </>
+            )}
+          </div>
+          <div className="flex-1 flex items-center justify-end space-x-0.5">
+            {room.userCount > 0 ? (
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+              </svg>
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+            )}
+            <span className="mt-0.5">
+              {room.userCount}{' / '}{room.userMax}
+            </span>
+          </div>
         </div>
       </div>
       <Separator />
